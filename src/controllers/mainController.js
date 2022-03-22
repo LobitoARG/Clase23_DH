b85658f7eda928c298400b677550f6aa07aa6c97
unknown <hjabo97@gmail.com>
2022-03-21 21:39:01 -0300

    falta el validator avanzado

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const {check} = require('express-validator');
-let validateRegister = [];
+
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
@@ -22,12 +21,7 @@ const controller = {
 	
 	datos: (req, res) =>{
 		var datosObtenidos = req.body;
-		const validateRegister = [
-			check('name').notEmpty().withMessage('Debes completar el nombre'),
-			check('color').notEmpty().withMessage('Debes seleccionar un color'),
-			//check('edad').notEmpty().withMessage('Debes ingresar un color').bail().isInt().withMessage('Debes ingresar con una edad valida'),
-			check('email').notEmpty().withMessage('Debes completar el email').bail().isEmail().withMessage('Debes completar un email valido')
-		]
+		
 		res.render('home', {datosObtenidos});
 	}
 
